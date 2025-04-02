5577fc140d8df84903d5aa3cc6f8e9b4a00b6793
adihatake <65528775+adihatake@users.noreply.github.com>
2025-04-02 16:54:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import time
 import Core_functs as bot
+import asyncio
 
 RUNNING = True
 PICKING_UP = True
 DELIVERING = True
 
 
-def main():  
+def Main():  
     bot.search_line()
     bot.turn_right()
   
@@ -21,6 +22,62 @@ def main():
             elif state == "obstacle detected":
                 bot.navigate_obstacle()
 
-main()
+
+async def main(right_adjust, left_adjust):
+    cancel_event = asyncio.Event()  # Event to signal task cancellation
+
+    task1 = asyncio.create_task(bot.move_forward(run_time=2, right_power=right_adjust, left_power=left_adjust, cancel_event=cancel_event))  # Start the motor movement asynchronously
+    task2 = asyncio.create_task(bot.read_IMU(cancel_event=cancel_event))  # Start IMU readings asynchronously
+
+
+    try:
+        await task1
+    except asyncio.CancelledError:
+        print("Motor task was cancelled.")
+    
+    try:
+        await task2
+    except asyncio.CancelledError:
+        print("IMU reading task was cancelled.")
+
+
+
+with open('/imu_data.txt','a') as file:
+            file.write("New Recording: \n")
+            file.close()
+
+
+# Run the event loop
+right_power = 80
+left_power = 75.5
+
+while True:
+    asyncio.run(main(right_power, left_power))
+    
+    
+    with open("imu_correction_data.txt", "r") as file:
+        correction = file.read()
+        
+    if correction == "left":
+        right_power = 90
+        left_power = 75.5
+        
+    if correction == "right":
+        right_power = 80
+        left_power = 85.5
+        
+    with open("imu_correction_data.txt", "w") as file:
+            file.write("")
+            file.close()
+        
+
+        
+        
+
+
+
+
+
+    
 
 
